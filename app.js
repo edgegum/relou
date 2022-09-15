0d7ba17d1133dca1948e395d00491d59d5768f1f
edgegum <104591550+edgegum@users.noreply.github.com>
2022-09-15 16:06:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.use(function run() {
+  fetch("/api/movie")
+    .then((response) => response.json())
+    .then((data) => {
+      const detailsElement = document.getElementById("movie");
+      //const detailsElement = document.getElementById("restaurants");
+      detailsElement.getElementsByTagName("img")[0].src = data.poster;
+      detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
+      detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
+      //detailsElement.getElementsByTagName("img")[0].src = data.poster;
+      //detailsElement.getElementsByTagName("h1")[0].innerText = data.cuisine;
+      //detailsElement.getElementsByTagName("p")[0].innerText = data.name;
+
+      detailsElement.style.visibility = "visible";
+    });
+}
+
 
 const html = `
 <!DOCTYPE html>
