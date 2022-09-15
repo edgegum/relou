dcd41b74e217cf36e6178cd3ffd56da614a386ef
edgegum <104591550+edgegum@users.noreply.github.com>
2022-09-15 18:06:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,23 +11,6 @@ app.use(function (req, res, next) {
   next()
 });
 
-app.use(function run() {
-  fetch("/api/movie")
-    .then((response) => response.json())
-    .then((data) => {
-      const detailsElement = document.getElementById("movie");
-      //const detailsElement = document.getElementById("restaurants");
-      detailsElement.getElementsByTagName("img")[0].src = data.poster;
-      detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
-      detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
-      //detailsElement.getElementsByTagName("img")[0].src = data.poster;
-      //detailsElement.getElementsByTagName("h1")[0].innerText = data.cuisine;
-      //detailsElement.getElementsByTagName("p")[0].innerText = data.name;
-
-      detailsElement.style.visibility = "visible";
-    });
-
-
 
 const html = `
 <!DOCTYPE html>
