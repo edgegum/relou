c8322c90507b1553bad569405f3ed35325ccd7d2
edgegum <104591550+edgegum@users.noreply.github.com>
2022-09-15 12:23:55 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+function run() {
+  fetch("/api/movie")
+    .then((response) => response.json())
+    .then((data) => {
+      const detailsElement = document.getElementById("movie");
+      //const detailsElement = document.getElementById("restaurants");
+      detailsElement.getElementsByTagName("img")[0].src = data.poster;
+      detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
+      detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
+      //detailsElement.getElementsByTagName("img")[0].src = data.poster;
+      //detailsElement.getElementsByTagName("h1")[0].innerText = data.cuisine;
+      //detailsElement.getElementsByTagName("p")[0].innerText = data.name;
+
+      detailsElement.style.visibility = "visible";
+    });
+}
