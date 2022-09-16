b9ad0322589406459587b22a45dba4c725b8571a
edgegum <104591550+edgegum@users.noreply.github.com>
2022-09-16 00:24:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const ejs =require("ejs");
+
 //app.set('IP','');
 
 app.get("/", (req, res) => res.type('html').send(html));
